c0ce050d493d4c300af44512ac31e6f2d34d5df9
Chila29 <86872622+Chila29@users.noreply.github.com>
2021-11-24 10:42:51 +0700

    update koneksi database

--- component/connectdb.inc.php
+++ component/connectdb.inc.php
@@ -1,3 +1,16 @@
 <?php
-    $conn = mysqli_init(); mysqli_real_connect($conn, "remotelab.mysql.database.azure.com", "cypr7128", "$rootr345", "remotelab", 3306, MYSQLI_CLIENT_SSL);;
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:motorlab.database.windows.net,1433; Database = motorlab", "admin_remlab", "Remote12");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "admin_remlab", "pwd" => "Remote12", "Database" => "motorlab", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:motorlab.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
