edb406e21c5504431ac3175aa4c56371464d391a
Chila29 <86872622+Chila29@users.noreply.github.com>
2021-11-24 09:45:44 +0700

    Koneksi database

--- component/connectdb.inc.php
+++ component/connectdb.inc.php
@@ -1,3 +1,3 @@
 <?php
-    $conn = mysqli_connect("localhost","root","","remotlab");
+    $conn = mysqli_init(); mysqli_real_connect($conn, "remotelab.mysql.database.azure.com", "cypr7128", "$rootr345", "remotelab", 3306, MYSQLI_CLIENT_SSL);;
 ?>
